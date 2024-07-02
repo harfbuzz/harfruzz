c07deff503f5bbe2bbc7a1ddf9d51cba1a835ff9
Chad Brokaw <cbrokaw@gmail.com>
2024-07-02 10:51:47 -0400

    use read-fonts for cmap

--- src/hb/face.rs
+++ src/hb/face.rs
@@ -12,20 +12,6 @@ use super::ot_layout::TableIndex;
 use super::ot_layout_common::{PositioningTable, SubstitutionTable};
 use crate::Variation;
 
-// https://docs.microsoft.com/en-us/typography/opentype/spec/cmap#windows-platform-platform-id--3
-const WINDOWS_SYMBOL_ENCODING: u16 = 0;
-const WINDOWS_UNICODE_BMP_ENCODING: u16 = 1;
-const WINDOWS_UNICODE_FULL_ENCODING: u16 = 10;
-
-// https://docs.microsoft.com/en-us/typography/opentype/spec/name#platform-specific-encoding-and-language-ids-unicode-platform-platform-id--0
-const UNICODE_1_0_ENCODING: u16 = 0;
-const UNICODE_1_1_ENCODING: u16 = 1;
-const UNICODE_ISO_ENCODING: u16 = 2;
-const UNICODE_2_0_BMP_ENCODING: u16 = 3;
-const UNICODE_2_0_FULL_ENCODING: u16 = 4;
-//const UNICODE_VARIATION_ENCODING: u16 = 5;
-const UNICODE_FULL_ENCODING: u16 = 6;
-
 /// A font face handle.
 #[derive(Clone)]
 pub struct hb_font_t<'a> {
@@ -34,7 +20,6 @@ pub struct hb_font_t<'a> {
     pub(crate) units_per_em: u16,
     pixels_per_em: Option<(u16, u16)>,
     pub(crate) points_per_em: Option<f32>,
-    prefered_cmap_encoding_subtable: Option<u16>,
     pub(crate) gsub: Option<SubstitutionTable<'a>>,
     pub(crate) gpos: Option<PositioningTable<'a>>,
 }
@@ -81,7 +66,6 @@ impl<'a> hb_font_t<'a> {
             units_per_em: face.units_per_em(),
             pixels_per_em: None,
             points_per_em: None,
-            prefered_cmap_encoding_subtable: find_best_cmap_subtable(&face),
             gsub: face.tables().gsub.map(SubstitutionTable::new),
             gpos: face.tables().gpos.map(PositioningTable::new),
             ttfp_face: face,
@@ -98,7 +82,6 @@ impl<'a> hb_font_t<'a> {
             units_per_em: face.units_per_em(),
             pixels_per_em: None,
             points_per_em: None,
-            prefered_cmap_encoding_subtable: find_best_cmap_subtable(&face),
             gsub: face.tables().gsub.map(SubstitutionTable::new),
             gpos: face.tables().gpos.map(PositioningTable::new),
             ttfp_face: face,
@@ -149,35 +132,14 @@ impl<'a> hb_font_t<'a> {
         self.get_nominal_glyph(c).is_some()
     }
 
-    pub(crate) fn get_nominal_glyph(&self, mut c: u32) -> Option<GlyphId> {
-        let subtable_idx = self.prefered_cmap_encoding_subtable?;
-        let subtable = self.tables().cmap?.subtables.get(subtable_idx)?;
+    pub(crate) fn get_nominal_glyph(&self, c: u32) -> Option<GlyphId> {
+        self.font.nominal_glyph(c).map(|gid| GlyphId(gid.to_u16()))
+    }
 
-        if subtable.platform_id == ttf_parser::PlatformId::Macintosh && c > 0x7F {
-            c = unicode_to_macroman(c);
-        }
-
-        match subtable.glyph_index(c) {
-            Some(gid) => Some(gid),
-            None => {
-                // Special case for Windows Symbol fonts.
-                // TODO: add tests
-                if subtable.platform_id == ttf_parser::PlatformId::Windows
-                    && subtable.encoding_id == WINDOWS_SYMBOL_ENCODING
-                {
-                    if c <= 0x00FF {
-                        // For symbol-encoded OpenType fonts, we duplicate the
-                        // U+F000..F0FF range at U+0000..U+00FF.  That's what
-                        // Windows seems to do, and that's hinted about at:
-                        // https://docs.microsoft.com/en-us/typography/opentype/spec/recom
-                        // under "Non-Standard (Symbol) Fonts".
-                        return self.get_nominal_glyph(0xF000 + c);
-                    }
-                }
-
-                None
-            }
-        }
+    pub(crate) fn glyph_variation_index(&self, c: char, vs: char) -> Option<GlyphId> {
+        self.font
+            .nominal_variant_glyph(c as u32, vs as u32)
+            .map(|gid| GlyphId(gid.to_u16()))
     }
 
     pub(crate) fn glyph_h_advance(&self, glyph: GlyphId) -> i32 {
@@ -389,67 +351,4 @@ pub struct hb_glyph_extents_t {
     pub y_bearing: i32,
     pub width: i32,
     pub height: i32,
-}
-
-fn find_best_cmap_subtable(face: &ttf_parser::Face) -> Option<u16> {
-    use ttf_parser::PlatformId;
-
-    // Symbol subtable.
-    // Prefer symbol if available.
-    // https://github.com/harfbuzz/harfbuzz/issues/1918
-    find_cmap_subtable(face, PlatformId::Windows, WINDOWS_SYMBOL_ENCODING)
-        // 32-bit subtables:
-        .or_else(|| find_cmap_subtable(face, PlatformId::Windows, WINDOWS_UNICODE_FULL_ENCODING))
-        .or_else(|| find_cmap_subtable(face, PlatformId::Unicode, UNICODE_FULL_ENCODING))
-        .or_else(|| find_cmap_subtable(face, PlatformId::Unicode, UNICODE_2_0_FULL_ENCODING))
-        // 16-bit subtables:
-        .or_else(|| find_cmap_subtable(face, PlatformId::Windows, WINDOWS_UNICODE_BMP_ENCODING))
-        .or_else(|| find_cmap_subtable(face, PlatformId::Unicode, UNICODE_2_0_BMP_ENCODING))
-        .or_else(|| find_cmap_subtable(face, PlatformId::Unicode, UNICODE_ISO_ENCODING))
-        .or_else(|| find_cmap_subtable(face, PlatformId::Unicode, UNICODE_1_1_ENCODING))
-        .or_else(|| find_cmap_subtable(face, PlatformId::Unicode, UNICODE_1_0_ENCODING))
-        // MacRoman subtable:
-        .or_else(|| find_cmap_subtable(face, PlatformId::Macintosh, 0))
-}
-
-fn find_cmap_subtable(
-    face: &ttf_parser::Face,
-    platform_id: ttf_parser::PlatformId,
-    encoding_id: u16,
-) -> Option<u16> {
-    for (i, subtable) in face.tables().cmap?.subtables.into_iter().enumerate() {
-        if subtable.platform_id == platform_id && subtable.encoding_id == encoding_id {
-            return Some(i as u16);
-        }
-    }
-
-    None
-}
-
-#[rustfmt::skip]
-static UNICODE_TO_MACROMAN: &[u16] = &[
-    0x00C4, 0x00C5, 0x00C7, 0x00C9, 0x00D1, 0x00D6, 0x00DC, 0x00E1,
-    0x00E0, 0x00E2, 0x00E4, 0x00E3, 0x00E5, 0x00E7, 0x00E9, 0x00E8,
-    0x00EA, 0x00EB, 0x00ED, 0x00EC, 0x00EE, 0x00EF, 0x00F1, 0x00F3,
-    0x00F2, 0x00F4, 0x00F6, 0x00F5, 0x00FA, 0x00F9, 0x00FB, 0x00FC,
-    0x2020, 0x00B0, 0x00A2, 0x00A3, 0x00A7, 0x2022, 0x00B6, 0x00DF,
-    0x00AE, 0x00A9, 0x2122, 0x00B4, 0x00A8, 0x2260, 0x00C6, 0x00D8,
-    0x221E, 0x00B1, 0x2264, 0x2265, 0x00A5, 0x00B5, 0x2202, 0x2211,
-    0x220F, 0x03C0, 0x222B, 0x00AA, 0x00BA, 0x03A9, 0x00E6, 0x00F8,
-    0x00BF, 0x00A1, 0x00AC, 0x221A, 0x0192, 0x2248, 0x2206, 0x00AB,
-    0x00BB, 0x2026, 0x00A0, 0x00C0, 0x00C3, 0x00D5, 0x0152, 0x0153,
-    0x2013, 0x2014, 0x201C, 0x201D, 0x2018, 0x2019, 0x00F7, 0x25CA,
-    0x00FF, 0x0178, 0x2044, 0x20AC, 0x2039, 0x203A, 0xFB01, 0xFB02,
-    0x2021, 0x00B7, 0x201A, 0x201E, 0x2030, 0x00C2, 0x00CA, 0x00C1,
-    0x00CB, 0x00C8, 0x00CD, 0x00CE, 0x00CF, 0x00CC, 0x00D3, 0x00D4,
-    0xF8FF, 0x00D2, 0x00DA, 0x00DB, 0x00D9, 0x0131, 0x02C6, 0x02DC,
-    0x00AF, 0x02D8, 0x02D9, 0x02DA, 0x00B8, 0x02DD, 0x02DB, 0x02C7,
-];
-
-fn unicode_to_macroman(c: u32) -> u32 {
-    let u = c as u16;
-    let Some(index) = UNICODE_TO_MACROMAN.iter().position(|m| *m == u) else {
-        return 0;
-    };
-    (0x7F + index) as u32
 }
--- src/hb/fonta/mod.rs
+++ src/hb/fonta/mod.rs
@@ -1,79 +1,8 @@
 pub mod ot;
+
+mod font;
 mod set_digest;
 
-use alloc::vec::Vec;
 use set_digest::SetDigest;
-use skrifa::{
-    charmap::Charmap,
-    instance::NormalizedCoord,
-    raw::{
-        tables::{
-            gpos::{AnchorTable, DeviceOrVariationIndex},
-            variations::{DeltaSetIndex, ItemVariationStore},
-        },
-        ReadError,
-    },
-    MetadataProvider,
-};
-use ttf_parser::NormalizedCoordinate;
 
-#[derive(Clone)]
-pub struct Font<'a> {
-    pub charmap: Charmap<'a>,
-    pub ot: ot::LayoutTables<'a>,
-    pub coords: Vec<NormalizedCoord>,
-    pub ivs: Option<ItemVariationStore<'a>>,
-}
-
-impl<'a> Font<'a> {
-    pub fn new(data: &'a [u8], font_index: u32) -> Option<Self> {
-        let font = skrifa::FontRef::from_index(data, font_index).ok()?;
-        let charmap = font.charmap();
-        let ot = ot::LayoutTables::new(&font);
-        Some(Self {
-            charmap,
-            ot,
-            coords: Vec::new(),
-            ivs: None,
-        })
-    }
-
-    pub(crate) fn set_coords(&mut self, coords: &[NormalizedCoordinate]) {
-        self.coords.clear();
-        if !coords.is_empty() && !coords.iter().all(|coord| coord.get() == 0) {
-            let ivs = self.ivs.take().or_else(|| self.ot.item_variation_store());
-            if ivs.is_some() {
-                self.coords.extend(
-                    coords
-                        .iter()
-                        .map(|coord| NormalizedCoord::from_bits(coord.get())),
-                );
-            }
-            self.ivs = ivs;
-        } else {
-            self.ivs = None;
-        }
-    }
-
-    fn resolve_anchor(&self, anchor: &AnchorTable) -> (i32, i32) {
-        let mut x = anchor.x_coordinate() as i32;
-        let mut y = anchor.y_coordinate() as i32;
-        if let Some(ivs) = self.ivs.as_ref() {
-            let delta = |val: Option<Result<DeviceOrVariationIndex<'_>, ReadError>>| match val {
-                Some(Ok(DeviceOrVariationIndex::VariationIndex(varix))) => ivs
-                    .compute_delta(
-                        DeltaSetIndex {
-                            outer: varix.delta_set_outer_index(),
-                            inner: varix.delta_set_inner_index(),
-                        },
-                        &self.coords,
-                    )
-                    .unwrap_or_default(),
-                _ => 0,
-            };
-            x += delta(anchor.x_device());
-            y += delta(anchor.y_device());
-        }
-        (x, y)
-    }
-}
+pub use font::Font;
