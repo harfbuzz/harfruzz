577da4977c8f3601cb1737d93f3ec2c2c48401d0
Rod S <rsheeter@google.com>
2024-07-25 20:19:33 -0300

    Move to the latest released version of Skrifa instead of a git dep

--- src/hb/fonta/mod.rs
+++ src/hb/fonta/mod.rs
@@ -3,6 +3,4 @@ pub mod ot;
 mod font;
 mod set_digest;
 
-use set_digest::SetDigest;
-
 pub use font::Font;
--- src/hb/fonta/ot/gsub/single.rs
+++ src/hb/fonta/ot/gsub/single.rs
@@ -5,17 +5,18 @@ use ttf_parser::GlyphId;
 
 impl WouldApply for SingleSubstFormat1<'_> {
     fn would_apply(&self, ctx: &WouldApplyContext) -> bool {
+        let gid = skrifa::GlyphId::from(ctx.glyphs[0].0);
         ctx.glyphs.len() == 1
             && self
                 .coverage()
-                .map(|cov| cov.get(ctx.glyphs[0].0.into()).is_some())
+                .map(|cov| cov.get(gid).is_some())
                 .unwrap_or_default()
     }
 }
 
 impl Apply for SingleSubstFormat1<'_> {
     fn apply(&self, ctx: &mut hb_ot_apply_context_t) -> Option<()> {
-        let glyph = ctx.buffer.cur(0).as_skrifa_glyph();
+        let glyph = ctx.buffer.cur(0).as_skrifa_glyph16();
         self.coverage().ok()?.get(glyph)?;
         let subst = (glyph.to_u16() as i32 + self.delta_glyph_id() as i32) as u16;
         ctx.replace_glyph(GlyphId(subst));
