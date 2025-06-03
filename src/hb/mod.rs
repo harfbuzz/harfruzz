2e03555d72596610ef49ed7f67a8519e410cb604
Chad Brokaw <cbrokaw@gmail.com>
2025-06-03 15:55:21 -0400

    Remove wasm shaper
    As mentioned in #45, this doesn't currently build and we can't actually support the required functionality with read-fonts (requires skrifa for loading outlines).
    
    We might consider exposing whatever is necessary to enable building this as a separate crate.

--- src/hb/mod.rs
+++ src/hb/mod.rs
@@ -70,8 +70,6 @@ mod aat_layout_common;
 mod ot_shaper_vowel_constraints;
 mod set_digest;
 pub mod shape;
-#[cfg(feature = "wasm-shaper")]
-mod shape_wasm;
 mod tag;
 mod tag_table;
 mod text_parser;
